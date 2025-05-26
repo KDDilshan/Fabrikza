643092491f2307f11369e6896f6dbc8845776e05
KDDilshan <kdprojects847@gmail.com>
2025-05-27 02:05:16 +0530

    added intial project

---
+++ src/test/java/com/kavindu/fabrikza/FabrikzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kavindu.fabrikza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabrikzaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
