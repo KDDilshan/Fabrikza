f9f46e8d04b42f5fcaf3d3809388855e0ec5a596
KDDilshan <kdprojects847@gmail.com>
2025-05-29 23:37:15 +0530

    add: separtte service class for handle product add logic

---
+++ src/main/java/com/kavindu/fabrikza/product/services/AddProductService.java
@@ -0,0 +1,12 @@
+package com.kavindu.fabrikza.product.services;
+
+import com.kavindu.fabrikza.product.dtos.Request.AddProductDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddProductService {
+
+    public void addProduct(AddProductDto addProductDto) {
+
+    }
+}
