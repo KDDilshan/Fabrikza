5abce51869570d07e3d50dbec1bbcb9e048de09d
KDDilshan <kdprojects847@gmail.com>
2025-05-29 23:35:42 +0530

    add: add product dto to add product detsis

---
+++ src/main/java/com/kavindu/fabrikza/product/dtos/Request/AddProductDto.java
@@ -0,0 +1,11 @@
+package com.kavindu.fabrikza.product.dtos.Request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddProductDto {
+}
