59c9c4c8284df7365b13a1f989186b2a280f1f4d
KDDilshan <kdprojects847@gmail.com>
2025-05-29 22:47:21 +0530

    updated: product entity to percise manner

--- src/main/java/com/kavindu/fabrikza/product/models/Product.java
+++ src/main/java/com/kavindu/fabrikza/product/models/Product.java
@@ -17,51 +17,63 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
+
     @Id
     @UuidGenerator
+    @Column(nullable = false, updatable = false)
     private UUID id;
 
+    @Column(nullable = false, length = 255)
     private String name;
 
+    @Column(length = 255)
     private String description;
 
+    @Column(nullable = false)
     private Double price;
 
+    @Column(length = 255)
     private String manufacturer;
 
+    @Column(length = 255)
     private String imageName;
 
     private Double discountPercentage;
 
+    @Temporal(TemporalType.TIMESTAMP)
     private Date discountStartDate;
 
+    @Temporal(TemporalType.TIMESTAMP)
     private Date discountEndDate;
 
-
-
     @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(updatable = false)
     private Date created_at;
 
     @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
     private Date updated_at;
 
-    @OneToMany(mappedBy = "product")
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ProductSize> sizes;
 
-    @OneToMany(mappedBy = "product")
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ProductColor> colors;
 
-    @OneToMany(mappedBy = "product")
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ProductImage> images;
 
-    @ManyToOne
+    @ManyToOne(optional = false)
     @JoinColumn(name = "category_id", nullable = false)
     private Category category;
 
-    @ManyToMany
-    @JoinTable(name = "product_tags",
-            joinColumns = @JoinColumn(name = "product_id"),
-            inverseJoinColumns = @JoinColumn(name = "tag_id"))
-    private List<Tag> tags;
 
+    @ManyToMany
+    @JoinTable(
+            name = "product_tags",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )
+    private List<Tag> tags;
 }
