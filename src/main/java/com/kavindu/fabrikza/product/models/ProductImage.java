a0121b1ce8b1de26db6d2061a99e14cb1a2e2803
KDDilshan <kdprojects847@gmail.com>
2025-05-30 13:46:55 +0530

    modified: product image model

--- src/main/java/com/kavindu/fabrikza/product/models/ProductImage.java
+++ src/main/java/com/kavindu/fabrikza/product/models/ProductImage.java
@@ -1,14 +1,12 @@
 package com.kavindu.fabrikza.product.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "product_image")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,8 +15,15 @@ public class ProductImage {
     @GeneratedValue
     private int id;
 
+    @Column(nullable = false)
     private String url;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", insertable = false, updatable = false)
     private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "color_id", insertable = false, updatable = false)
+    private Color color;
+
 }
