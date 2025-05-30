90e6f65b880e6211b5ada40e690fb870f4e3c619
KDDilshan <kdprojects847@gmail.com>
2025-05-30 13:25:00 +0530

    add: reponse dto

---
+++ src/main/java/com/kavindu/fabrikza/product/dtos/Response/ReponseDto.java
@@ -0,0 +1,13 @@
+package com.kavindu.fabrikza.product.dtos.Response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ReponseDto {
+}
