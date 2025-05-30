35fbfcb0e8eeaa1fb819880b95c3486290e464d6
KDDilshan <kdprojects847@gmail.com>
2025-05-30 13:46:35 +0530

    add product repo

--- src/main/java/com/kavindu/fabrikza/product/repositories/ProductRepository.java
+++ src/main/java/com/kavindu/fabrikza/product/repositories/ProductRepository.java
@@ -1,4 +1,11 @@
 package com.kavindu.fabrikza.product.repositories;
 
-public class ProductRepository {
+import com.kavindu.fabrikza.product.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository  extends JpaRepository<Product, UUID> {
 }
