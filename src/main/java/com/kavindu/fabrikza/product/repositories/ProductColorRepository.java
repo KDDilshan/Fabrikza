bd178bb7e4565883827f3a9e84b8aaa8549cfce6
KDDilshan <kdprojects847@gmail.com>
2025-06-01 22:31:42 +0530

    add: product repository

--- src/main/java/com/kavindu/fabrikza/product/repositories/ProductColorRepository.java
+++ src/main/java/com/kavindu/fabrikza/product/repositories/ProductColorRepository.java
@@ -4,6 +4,11 @@ import com.kavindu.fabrikza.product.models.Color;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductColorRepository extends JpaRepository<Color,Integer> {
+    Optional<Color> findByNameIgnoreCase(String name);
+    Optional<Color> findByHexCode(String hexCode);
+    Optional<Color> findByNameIgnoreCaseAndHexCode(String name, String hexCode);
 }
