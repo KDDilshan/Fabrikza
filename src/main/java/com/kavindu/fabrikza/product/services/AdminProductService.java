f1b8ab926f209809eb421d6652873d073b032912
KDDilshan <kdprojects847@gmail.com>
2025-06-01 22:19:28 +0530

    rename file: to admin product service to separet what admin can do what user can do services

---
+++ src/main/java/com/kavindu/fabrikza/product/services/AdminProductService.java
@@ -0,0 +1,122 @@
+package com.kavindu.fabrikza.product.services;
+
+import com.kavindu.fabrikza.product.dtos.Request.*;
+import com.kavindu.fabrikza.product.dtos.Response.ProductResponseDTO;
+import com.kavindu.fabrikza.product.mapper.ProductMapper;
+import com.kavindu.fabrikza.product.models.*;
+import com.kavindu.fabrikza.product.repositories.*;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class AdminProductService {
+
+    private final ProductRepository productRepository;
+    private final ProductColorRepository productColorRepository;
+    private final ProductSizeRepository productSizeRepository;
+    private final ProductImageRepository productImageRepository;
+    private final CategoryRepository categoryRepository;
+    private final ProductVariantsRepository productVariantsRepository;
+    private final ProductMapper productMapper;
+
+    public AdminProductService(ProductRepository productRepository, ProductColorRepository productColorRepository, ProductSizeRepository productSizeRepository, ProductImageRepository productImageRepository, CategoryRepository categoryRepository, ProductVariantsRepository productVariantsRepository, ProductMapper productMapper) {
+        this.productRepository = productRepository;
+        this.productColorRepository = productColorRepository;
+        this.productSizeRepository = productSizeRepository;
+        this.productImageRepository = productImageRepository;
+        this.categoryRepository = categoryRepository;
+        this.productVariantsRepository = productVariantsRepository;
+        this.productMapper = productMapper;
+    }
+
+    public void addProduct(AddProductDto addProductDto) {
+
+        Category category = categoryRepository.findById(addProductDto.getCategory().getId())
+                .orElseThrow(() -> new RuntimeException("Category not found"));
+
+        Product product=new Product();
+        product.setName(addProductDto.getName());
+        product.setDescription(addProductDto.getDescription());
+        product.setManufacturer(addProductDto.getManufacturer());
+        product.setPrice(addProductDto.getPrice());
+        product.setCategory(category);
+
+        Product savedProduct = productRepository.save(product);
+
+        for(ProductColorDTO colorDTO:addProductDto.getColors()){
+            Color color= productColorRepository
+                    .findByNameIgnoreCaseAndHexCode(colorDTO.getColorName(),colorDTO.getHexCode())
+                    .orElse(null);
+
+            if(color == null ){
+                color=new Color();
+                color.setName(colorDTO.getColorName());
+                color.setHexCode(colorDTO.getHexCode());
+                color=productColorRepository.save(color);
+            }
+
+            for (ProductImageDTO imageDto : colorDTO.getImages()) {
+                ProductImage productImage = new ProductImage();
+                productImage.setUrl(imageDto.getUrl());
+                productImage.setProduct(product);
+                productImage.setColor(color);
+
+                productImageRepository.save(productImage);
+            }
+
+            for(ProductVariantDTO variantDTO: colorDTO.getVariants()){
+                Size size = productSizeRepository.findByLabel(variantDTO.getSizeLabel())
+                        .orElse(null);
+
+                if (size == null) {
+                    size = new Size();
+                    size.setLabel(variantDTO.getSizeLabel());
+                    size = productSizeRepository.save(size);
+                }
+
+                ProductVariants variant = new ProductVariants();
+                variant.setProduct(product);
+                variant.setColor(color);
+                variant.setSize(size);
+                variant.setQuantity(variantDTO.getQuantity());
+                productVariantsRepository.save(variant);
+
+            }
+
+        }
+
+    }
+
+    public ProductResponseDTO updateProduct(UUID id, UpdateProductDto updateDto) {
+        Product product=productRepository.findById(id)
+                .orElseThrow(()->new RuntimeException("Product not found"));
+
+        if (updateDto.getName() != null) {
+            product.setName(updateDto.getName());
+        }
+        if (updateDto.getDescription() != null) {
+            product.setDescription(updateDto.getDescription());
+        }
+        if (updateDto.getPrice() != null) {
+            product.setPrice(updateDto.getPrice());
+        }
+        if (updateDto.getManufacturer() != null) {
+            product.setManufacturer(updateDto.getManufacturer());
+        }
+        if (updateDto.getCategoryId() != null) {
+            Category category = categoryRepository.findById(updateDto.getCategoryId())
+                    .orElseThrow(() -> new RuntimeException("Category not found"));
+            product.setCategory(category);
+        }
+        product = productRepository.save(product);
+        return productMapper.toResponseDTO(product);
+    }
+
+    public void deleteProduct(UUID id) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+
+        productRepository.delete(product);
+    }
+}
