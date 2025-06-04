6a411572994ddf743fa38e42c832d2992d642379
KDDilshan <kdprojects847@gmail.com>
2025-06-04 08:42:17 +0530

    update: user product service for serch products and sort products by price

--- src/main/java/com/kavindu/fabrikza/product/services/UserProductService.java
+++ src/main/java/com/kavindu/fabrikza/product/services/UserProductService.java
@@ -1,11 +1,14 @@
 package com.kavindu.fabrikza.product.services;
 
+import com.kavindu.fabrikza.product.dtos.Response.ProductListResponseDTO;
 import com.kavindu.fabrikza.product.dtos.Response.ProductResponseDTO;
 import com.kavindu.fabrikza.product.mapper.ProductMapper;
 import com.kavindu.fabrikza.product.models.Product;
 import com.kavindu.fabrikza.product.repositories.*;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,12 +32,43 @@ public class UserProductService {
         this.productMapper = productMapper;
     }
 
-    public List<ProductResponseDTO> getAllProducts() {
+    public List<ProductListResponseDTO> getAllProducts() {
         List<Product> products = productRepository.findAll();
         return products.stream()
-                .map(productMapper::toResponseDTO)
+                .map(productMapper::toNewDto)
                 .collect(Collectors.toList());
     }
 
 
+    public List<ProductListResponseDTO> searchProduct(String query, String color, String size) {
+        try{
+            if (query != null || color != null || size != null) {
+                List<Product> products =productRepository.searchByName(query);
+                return products.stream()
+                        .map(productMapper::toNewDto)
+                        .collect(Collectors.toList());
+            } else {
+                return getAllProducts();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
+    }
+
+    public List<ProductListResponseDTO> orderByPrice(String order) {
+        try {
+            Sort sort = order.equalsIgnoreCase("asc")
+                    ? Sort.by(Sort.Order.asc("price"))
+                    : Sort.by(Sort.Order.desc("price"));
+            List<Product> products = productRepository.findAll(sort);
+            return products.stream()
+                    .map(productMapper::toNewDto)
+                    .collect(Collectors.toList());
+        }catch (Exception e){
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
+
+    }
 }
