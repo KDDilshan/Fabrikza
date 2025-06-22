94c0e9b932bac5cb0f7cbefae40360cba1010220
KDDilshan <kdprojects847@gmail.com>
2025-06-22 21:12:13 +0530

    update: product image repositoies to add new metho to get prduct image from product and color id

--- src/main/java/com/kavindu/fabrikza/product/repositories/ProductImageRepository.java
+++ src/main/java/com/kavindu/fabrikza/product/repositories/ProductImageRepository.java
@@ -7,10 +7,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface ProductImageRepository extends JpaRepository<ProductImage,Integer> {
     List<ProductImage> findByProduct(Product product);
     List<ProductImage> findByColor(Color color);
     List<ProductImage> findByProductAndColor(Product product, Color color);
+
+    Optional<ProductImage> findFirstByProductIdAndColorId(UUID id, int id1);
 }
